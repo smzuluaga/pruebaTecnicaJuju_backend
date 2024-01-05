621d3057f23a28f2245811928ba72ee11fd06e00
smzuluaga01home <santimzuluaga01@hotmail.com>
2024-01-05 01:10:51 -0500

    ajustes en endpoints

--- src/routes/book.js
+++ src/routes/book.js
@@ -25,6 +25,37 @@ router.get('/books', (req,res) => {
 
 })
 
+//Find By Id
+router.get('/books/:id', (req, res) => {
+    const {id} = req.params;
+
+    bookSchema
+    .findById(id)
+    .then((data)=>res.json(data))
+    .catch((error)=> res.json({message: error}));
+})
+
+//Update By Id
+router.put('/books/:id', (req, res) => {
+    const {id} = req.params;
+    const {author, pubYear ,title,state,ownerId,currentHolderId} = req.body;
+
+    bookSchema
+    .updateOne({ _id:id}, {$set :{author, pubYear ,title,state,ownerId,currentHolderId}})
+    .then((data)=>res.json(data))
+    .catch((error)=> res.json({message: error}));
+})
+
+//delete By Id
+router.delete('/books/:id', (req, res) => {
+    const {id} = req.params;
+
+    bookSchema
+    .deleteOne({ _id:id})
+    .then((data)=>res.json(data))
+    .catch((error)=> res.json({message: error}));
+})
+
 
 
 module.exports = router;
